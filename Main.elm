6f6f8cc59aedc5aa835980f6a2afe613b86ea36a
Mayeu <m@mayeu.me>
2017-03-24 23:40:58 +0100

    Now with storage limit!

--- Main.elm
+++ Main.elm
@@ -15,6 +15,8 @@ type alias Model =
     , factory : NanomachineFactory
     , rawMaterials : Int
     , nanomachineCost : Int
+    , storageLevel : Int
+    , expandStorageCost : Int
     }
 
 
@@ -25,7 +27,7 @@ type alias NanomachineFactory =
 
 initialModel : Model
 initialModel =
-    Model 1 0 initialNanomachineFactory initialRawMaterials initialNonaMachineCost
+    Model 1 0 initialNanomachineFactory initialRawMaterials initialNonaMachineCost initialStorageLevel initialExpandStorageCost
 
 
 initialBuildTime =
@@ -40,6 +42,14 @@ initialNonaMachineCost =
     100
 
 
+initialStorageLevel =
+    0
+
+
+initialExpandStorageCost =
+    500
+
+
 initialNanomachineFactory : NanomachineFactory
 initialNanomachineFactory =
     NanomachineFactory initialBuildTime
@@ -52,13 +62,14 @@ initialNanomachineFactory =
 type Msg
     = Tick Time
     | ConvertNanomachine
+    | ExpandStorage
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick _ ->
-            ( { model | rawMaterials = model.rawMaterials + model.nanomachineQuantity * 1 }, Cmd.none )
+            ( { model | rawMaterials = updateRawMaterials model }, Cmd.none )
 
         ConvertNanomachine ->
             if model.rawMaterials >= model.nanomachineCost then
@@ -71,11 +82,46 @@ update msg model =
             else
                 ( model, Cmd.none )
 
+        ExpandStorage ->
+            if model.rawMaterials >= model.expandStorageCost then
+                ( { model
+                    | rawMaterials = model.rawMaterials - model.expandStorageCost
+                    , storageLevel = model.storageLevel + 1
+                  }
+                , Cmd.none
+                )
+            else
+                ( model, Cmd.none )
+
+
+updateRawMaterials : Model -> Int
+updateRawMaterials model =
+    let
+        newRawMaterials =
+            model.rawMaterials + model.nanomachineQuantity
+
+        totalStorage =
+            currentStorage model
+    in
+        if newRawMaterials > totalStorage then
+            totalStorage
+        else
+            newRawMaterials
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
         [ Time.every second Tick ]
+
+
+
+-- Helper function
+
+
+currentStorage : Model -> Int
+currentStorage model =
+    1000 + model.storageLevel * 200
 
 
 
@@ -92,6 +138,11 @@ materialInfo quantity =
     "Raw Materials: " ++ (toString quantity)
 
 
+storageInfo : Int -> String
+storageInfo quantity =
+    "Total Storage: " ++ (toString quantity)
+
+
 viewNanomachine : Int -> Html Msg
 viewNanomachine quantity =
     p [] [ text (nanomachineInfo quantity) ]
@@ -102,12 +153,19 @@ viewMaterials quantity =
     p [] [ text (materialInfo quantity) ]
 
 
+viewStorage : Int -> Html Msg
+viewStorage quantity =
+    p [] [ text (storageInfo quantity) ]
+
+
 view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ viewNanomachine model.nanomachineQuantity
         , viewMaterials model.rawMaterials
+        , currentStorage model |> viewStorage
         , div [] [ button [ onClick ConvertNanomachine ] [ text ("Convert nanomachine (100 raw materials)") ] ]
+        , div [] [ button [ onClick ExpandStorage ] [ text ("Expand Storage (500 raw materials)") ] ]
         ]
 
 
