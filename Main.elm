3fb5bbc7469b3994025cc6ee0d7ee52ecbb36835
Mayeu <m@mayeu.me>
2017-03-24 23:21:10 +0100

    You can buy nanomachine now

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ module GrayGoo exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Time exposing (Time, second)
 
 
@@ -9,15 +10,39 @@ import Time exposing (Time, second)
 
 
 type alias Model =
-    { gooQuantity : Int
+    { nanomachineQuantity : Int
     , lastUpdate : Time
-    , replicationTime : Float
+    , factory : NanomachineFactory
+    , rawMaterials : Int
+    , nanomachineCost : Int
+    }
+
+
+type alias NanomachineFactory =
+    { buildTime : Int
     }
 
 
 initialModel : Model
 initialModel =
-    Model 1 0 5
+    Model 1 0 initialNanomachineFactory initialRawMaterials initialNonaMachineCost
+
+
+initialBuildTime =
+    5
+
+
+initialRawMaterials =
+    0
+
+
+initialNonaMachineCost =
+    100
+
+
+initialNanomachineFactory : NanomachineFactory
+initialNanomachineFactory =
+    NanomachineFactory initialBuildTime
 
 
 
@@ -26,47 +51,64 @@ initialModel =
 
 type Msg
     = Tick Time
+    | ConvertNanomachine
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick _ ->
-            ( { model | gooQuantity = model.gooQuantity * 2 }
-            , Cmd.none
-            )
+            ( { model | rawMaterials = model.rawMaterials + model.nanomachineQuantity * 1 }, Cmd.none )
 
-
-
--- SUBSCRIPTIONS
+        ConvertNanomachine ->
+            if model.rawMaterials >= model.nanomachineCost then
+                ( { model
+                    | nanomachineQuantity = model.nanomachineQuantity + 1
+                    , rawMaterials = model.rawMaterials - model.nanomachineCost
+                  }
+                , Cmd.none
+                )
+            else
+                ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Time.every
-            (model.replicationTime * second)
-            Tick
-        ]
+        [ Time.every second Tick ]
 
 
 
 -- VIEW
 
 
-gooInfo : Int -> String
-gooInfo quantity =
+nanomachineInfo : Int -> String
+nanomachineInfo quantity =
     "Nanomachine: " ++ (toString quantity)
 
 
-viewGoo : Int -> Html Msg
-viewGoo quantity =
-    p [] [ text (gooInfo quantity) ]
+materialInfo : Int -> String
+materialInfo quantity =
+    "Raw Materials: " ++ (toString quantity)
+
+
+viewNanomachine : Int -> Html Msg
+viewNanomachine quantity =
+    p [] [ text (nanomachineInfo quantity) ]
+
+
+viewMaterials : Int -> Html Msg
+viewMaterials quantity =
+    p [] [ text (materialInfo quantity) ]
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ] [ viewGoo model.gooQuantity ]
+    div [ class "content" ]
+        [ viewNanomachine model.nanomachineQuantity
+        , viewMaterials model.rawMaterials
+        , div [] [ button [ onClick ConvertNanomachine ] [ text ("Convert nanomachine (100 raw materials)") ] ]
+        ]
 
 
 main : Program Never Model Msg
